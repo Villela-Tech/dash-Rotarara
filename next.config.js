22453508b3ba70367b47219d3424380c019f2eb7
Cássio Machado <139145831+cassmach@users.noreply.github.com>
2025-03-26 22:56:37 -0300

    fix: atualiza configurações para forçar rebuild no Netlify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@nivo/core', '@nivo/pie', '@nivo/bar', '@nivo/line']
+  transpilePackages: ['@nivo/core', '@nivo/pie', '@nivo/bar', '@nivo/line'],
+  output: 'standalone',
+  generateEtags: false,
+  poweredByHeader: false,
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig; 
